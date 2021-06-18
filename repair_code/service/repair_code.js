62c02aceb86e65a583536433d0f9207bfe9d2177
Nihal Pandey <nihalpandey@farmart.co>
2021-06-19 00:03:19 +0530

    decodeuricomponent added to solve param for code and test cases solved

--- repair_code/service/repair_code.js
+++ repair_code/service/repair_code.js
@@ -5,8 +5,10 @@ module.exports = async(code,test_cases,class_name)=>{
     return new Promise(async(resolve,reject)=>{
         try{
             //className should be same as fileName
-            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}.java`,code);
-            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}Test.java`,test_cases);
+            const decoded_code = decodeURIComponent(code);
+            const decoded_test_cases = decodeURIComponent(test_cases);
+            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}.java`,decoded_code);
+            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}Test.java`,decoded_test_cases);
             const initiate_command =`java -jar kGenProg.jar -r ./ -s kGenProg/example/repair_code_eg/src/example/${class_name}.java -t kGenProg/example/repair_code_eg/src/example/${class_name}Test.java --patch-output -o ./output`
             const response = await execute_shell(initiate_command);
             console.log(response);
