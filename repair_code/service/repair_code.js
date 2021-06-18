759309faeb004438cbeef2a01b692f261958fe73
Nihal Pandey <nihalpandey@farmart.co>
2021-06-19 00:00:39 +0530

    file upload folder solved

--- repair_code/service/repair_code.js
+++ repair_code/service/repair_code.js
@@ -5,12 +5,12 @@ module.exports = async(code,test_cases,class_name)=>{
     return new Promise(async(resolve,reject)=>{
         try{
             //className should be same as fileName
-            fs.writeFileSync(`resources/${class_name}.java`,code);
-            fs.writeFileSync(`resources/${class_name}Test.java`,test_cases);
-            const initiate_command = `java -jar kGenProg.jar -r ./ -s ./resources/${class_name}.java -t ./resources/${class_name}Test.java --patch-output -o ./output`
+            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}.java`,code);
+            fs.writeFileSync(`kGenProg/example/repair_code_eg/src/example/${class_name}Test.java`,test_cases);
+            const initiate_command =`java -jar kGenProg.jar -r ./ -s kGenProg/example/repair_code_eg/src/example/${class_name}.java -t kGenProg/example/repair_code_eg/src/example/${class_name}Test.java --patch-output -o ./output`
             const response = await execute_shell(initiate_command);
             console.log(response);
-            output_code = fs.readFileSync("output/resources.Codefile.java","utf8");  
+            output_code = fs.readFileSync(`output/patch-v7/example.${class_name}.java`,"utf8");  
             resolve(output_code);
         }
         catch(err){
@@ -18,3 +18,5 @@ module.exports = async(code,test_cases,class_name)=>{
         }
     })
 }
+
+//java -jar kGenProg.jar -r ./ -s kGenProg/example/repair_code_eg/CloseToZero.java -t kGenProg/example/repair_code_eg/CloseToZeroTest.java --patch-output -o ./output
