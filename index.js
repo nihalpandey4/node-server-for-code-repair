39052cc08b1ccc37925713188b2e1c052708aa69
Nihal Pandey <nihalpandey@farmart.co>
2021-06-19 22:53:07 +0530

    port changed

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const repair_code = require("./repair_code");
 
+const cors = require("cors");
+
 const app = express();
 
-//json request parsing 
 app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    credentials: true,
+  }));
+
+//json request parsing 
 
 repair_code(app);
 
-app.listen(3000,()=>{
-    console.log("server is running at port 3000");
+app.listen(3001,()=>{
+    console.log("server is running at port 3001");
 })
--- repair_code/service/repair_code.js
+++ repair_code/service/repair_code.js
@@ -12,7 +12,7 @@ module.exports = async(code,test_cases,class_name)=>{
             const initiate_command =`java -jar kGenProg.jar -r ./ -s kGenProg/example/repair_code_eg/src/example/${class_name}.java -t kGenProg/example/repair_code_eg/src/example/${class_name}Test.java --patch-output -o ./output`
             const response = await execute_shell(initiate_command);
             console.log(response);
-            output_code = fs.readFileSync(`output/patch-v7/example.${class_name}.java`,"utf8");  
+            output_code = await  fs.readFileSync(`output/patch-v7/example.${class_name}.java`,"utf8");  
             resolve(output_code);
         }
         catch(err){
